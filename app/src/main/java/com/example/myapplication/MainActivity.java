f2c77b47cde13a4657b649c22661a07ecacef075
popo12 <maor5599@gmail.con>
2024-03-12 21:14:23 +0200

    update

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,33 +1,35 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.InputType;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     SharedPreferences sp;
     SharedPreferences.Editor editor;
+    private FirebaseAuth auth;
     ToggleButton togglePassword;
 
     EditText userName, password;
-    TextView tv;
+    TextView tv,signup;
     CheckBox remember;
 
     @Override
@@ -42,11 +44,18 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         }
         initialization();
-
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Signup.class));
+            }
+        });
+        auth = FirebaseAuth.getInstance();
     }
     private void initialization()
     {
         userName = findViewById(R.id.userNameET);
+        signup = findViewById(R.id.signUpRedirectText);
         password = findViewById(R.id.passwordET);
         remember = findViewById(R.id.remember);
         tv = findViewById(R.id.error);
@@ -55,60 +64,36 @@ public class MainActivity extends AppCompatActivity {
         togglePassword.setTextOn(null);
     }
 
+
     public void login(View view) {
-        String enteredUsername = userName.getText().toString();
-        String enteredPassword = password.getText().toString();
-
-        // Query the Firebase database to check if the user exists
-        DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference("Users");
-        usersRef.orderByChild("UserName").equalTo(enteredUsername).addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()) {
-                    for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
-                        String storedPassword = userSnapshot.child("password").getValue(String.class);
-                        if (enteredPassword.equals(storedPassword)) {
-                            // Password matches, user is authenticated
-                            if(remember.isChecked())
-                            {
-                                sp = getSharedPreferences("checkBox",MODE_PRIVATE);
-                                editor = sp.edit();
-                                editor.putString("remember","true");
-                                editor.putString("name",enteredUsername);
-                                editor.putString("password",enteredPassword);
-                                editor.apply();
+        String email = userName.getText().toString();
+        String pass = password.getText().toString();
+        if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            if (!pass.isEmpty()) {
+                auth.signInWithEmailAndPassword(email, pass)
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(MainActivity.this, MainActivity2.class));
+                                finish();
                             }
-                            else
-                            {
-                                sp = getSharedPreferences("checkBox",MODE_PRIVATE);
-                                editor = sp.edit();
-                                editor.putString("remember","false");
-                                editor.apply();
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
                             }
-                            // Start the MainActivity2 activity
-                            Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-                            intent.putExtra("name",enteredUsername);
-                            intent.putExtra("password",enteredPassword);
-                            startActivity(intent);
-                            tv.setText("");
-                        } else {
-                            // Password does not match
-                            tv.setText("Incorrect password");
-                        }
-                    }
-                } else {
-                    // User with the entered username doesn't exist
-                    tv.setText("User not found");
-                }
+                        });
+            } else {
+                password.setError("Empty fields are not allowed");
             }
+        } else if (email.isEmpty()) {
+            userName.setError("Empty fields are not allowed");
+        } else {
+            userName.setError("Please enter correct email");
+        }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                // Handle database errors
-            }
-        });
     }
-
 
     public void TogglePassword(View view) {
         if (password.getInputType() == InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD) {
@@ -125,4 +110,6 @@ public class MainActivity extends AppCompatActivity {
     public void Forgot_password(View view) {
         startActivity(new Intent(getApplicationContext(), SendOTPActivity.class));
     }
+
+
 }
---
+++ app/src/main/java/com/example/myapplication/Signup.java
@@ -0,0 +1,67 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Signup extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private EditText signupEmail, signupPassword;
+    private Button signupButton;
+    private TextView loginRedirectText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        auth = FirebaseAuth.getInstance();
+        signupEmail = findViewById(R.id.signup_email);
+        signupPassword = findViewById(R.id.signup_password);
+        signupButton = findViewById(R.id.signup_button);
+        loginRedirectText = findViewById(R.id.loginRedirectText);
+
+    }
+
+    public void signupUser(View view) {
+        String user = signupEmail.getText().toString().trim();
+        String pass = signupPassword.getText().toString().trim();
+        if (user.isEmpty()){
+            signupEmail.setError("Email cannot be empty");
+        }
+        if (pass.isEmpty()){
+            signupPassword.setError("Password cannot be empty");
+        }
+        else if(!user.isEmpty()&&!pass.isEmpty()){
+            auth.createUserWithEmailAndPassword(user, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(Signup.this, "SignUp Successful", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(Signup.this, MainActivity.class));
+                    } else {
+                        Toast.makeText(Signup.this, "SignUp Failed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+        loginRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Signup.this, MainActivity.class));
+            }
+        });
+    }
+}
